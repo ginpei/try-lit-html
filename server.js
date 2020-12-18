46d94163e45ceb353ac04aeeb2720e1332825b64
ginpei <ginpei@gmail.com>
2020-12-18 14:08:49 -0800

    feat: 🎸 set up lit-html and dev server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.static(path.resolve("public")));
+
+app.listen(port, () => console.log(`Listening at ${port}`));
